519979f47e9702d2b7f1a5116f59142778a4abe5
Furkan <furkn.ceyln@gmail.com>
2022-07-06 18:07:20 +0300

    Initial Commit

---
+++ BankIdentifyNumberChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankIdentifyNumberChecker
+//
+//  Created by Furkan Ceylan on 6.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
